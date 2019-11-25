c0f97c022f7699316441f08d57cca844770d5ea6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-25 21:03:15 +0100

    Update SomethingRepositoryMockMvcTests.java

--- graph-neo4j/src/test/java/local/example/graph/repository/SomethingRepositoryMockMvcTests.java
+++ graph-neo4j/src/test/java/local/example/graph/repository/SomethingRepositoryMockMvcTests.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -33,6 +34,7 @@ class SomethingRepositoryMockMvcTests {
     @Autowired
     private MockMvc mockMvc;
 
+    /* it is not necessary that Neo4j community edition is in operation */
     @Test
     void sampleTest()
             throws Exception {
@@ -40,5 +42,120 @@ class SomethingRepositoryMockMvcTests {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$._links.everything").exists());
+    }
+
+    /* it's necessary that Neo4j community edition is correctly installed and in operation */
+    @Test
+    void existTest()
+            throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/everything"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.everything").exists());
+    }
+
+    /**
+     * from here on, it's necessary that Neo4j community edition is correctly installed,
+     * in operation and with some stored data
+     */
+    @Test
+    void retrieveExistTest()
+            throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/everything/22"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").exists());
+    }
+
+    @Test
+    void retrieveCodeValueTest()
+            throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/everything/22"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        if (location != null) {
+            mockMvc.perform(MockMvcRequestBuilders.get(location))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers
+                            .jsonPath("$.code").value("FE8NXS34M1W9"));
+        }
+    }
+
+    @Test
+    void existLinksTest()
+            throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/everything/22"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$._links").exists());
+    }
+
+    @Test
+    void existSelfLinkTest()
+            throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/everything/22"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$._links.self").exists());
+    }
+
+    @Test
+    void retrieveSelfLinkValueTest()
+            throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/everything/22"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        if (location != null) {
+            mockMvc.perform(MockMvcRequestBuilders.get(location))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers
+                            .jsonPath("$._links.self")
+                            .value("http://127.0.0.1:8080/everything/22"));
+        }
+    }
+
+    @Test
+    void retrieveSomethingLinkValueTest()
+            throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/everything/22"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        if (location != null) {
+            mockMvc.perform(MockMvcRequestBuilders.get(location))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers
+                            .jsonPath("$._links.something")
+                            .value("http://127.0.0.1:8080/everything/22"));
+        }
+    }
+
+    @Test
+    void retrieveFindByCodeTest()
+            throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("http://127.0.0.1:8080/everything/search/findByCode?code=FE8NXS34M1W9"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        if (location != null) {
+            mockMvc.perform(MockMvcRequestBuilders.get(location))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers
+                            .jsonPath("$._embedded.everything.thinks")
+                            .value(null))
+                    .andExpect(MockMvcResultMatchers
+                            .jsonPath("$._embedded.everything._links.self")
+                            .value("http://127.0.0.1:8080/everything/22"))
+                    .andExpect(MockMvcResultMatchers
+                            .jsonPath("$._embedded.everything._links.something")
+                            .value("http://127.0.0.1:8080/everything/22"));
+        }
     }
 }
