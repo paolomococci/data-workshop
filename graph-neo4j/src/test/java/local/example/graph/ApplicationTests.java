c0c82fab68912e4dbf96d9c9abc15b83bd04c0b9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-25 20:42:49 +0100

    Update ApplicationTests.java

--- graph-neo4j/src/test/java/local/example/graph/ApplicationTests.java
+++ graph-neo4j/src/test/java/local/example/graph/ApplicationTests.java
@@ -19,12 +19,28 @@
 package local.example.graph;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	/*  */
 	@Test
-	void someTest() {
+	void someTest()
+			throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$").exists());
 	}
 }
