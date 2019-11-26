815b2854e74bb940c7cfff7cc76e204e0351e000
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-11-26 10:02:32 +0100

    Create SomethingController.java

---
+++ graph-neo4j/src/main/java/local/example/graph/controller/SomethingController.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.graph.controller;
+
+import local.example.graph.service.SomethingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class SomethingController {
+
+    @Autowired
+    private SomethingService somethingService;
+
+    @GetMapping("/graph")
+    public Map<String, Object> graph(
+            @RequestParam(
+                    value = "limit",
+                    required = false
+            ) Integer limit) {
+        return somethingService.graphMap((limit == null) ? 50 : limit);
+    }
+}
