69933b27bd8068c0f829555541fb12ea7d4122d5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-03 21:43:53 +0100

    Update Book.java

--- library-database/src/main/java/local/example/data/model/Book.java
+++ library-database/src/main/java/local/example/data/model/Book.java
@@ -18,5 +18,23 @@
 
 package local.example.data.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    private long id;
+
+    @Getter
+    @Setter
+    private String title;
 }
