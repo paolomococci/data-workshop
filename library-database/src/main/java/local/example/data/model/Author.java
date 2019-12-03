6dd6e1483c5a7e94c54a09a8c8fefb2c4652ac46
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-03 21:43:25 +0100

    Update Author.java

--- library-database/src/main/java/local/example/data/model/Author.java
+++ library-database/src/main/java/local/example/data/model/Author.java
@@ -18,5 +18,27 @@
 
 package local.example.data.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    private long id;
+
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Getter
+    @Setter
+    private String lastName;
 }
