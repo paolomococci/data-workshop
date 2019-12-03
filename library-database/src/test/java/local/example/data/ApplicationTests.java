3797f407a67db43c50fb0c94f6571dde9f0ea01a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-03 15:53:44 +0100

    Update ApplicationTests.java

--- library-database/src/test/java/local/example/data/ApplicationTests.java
+++ library-database/src/test/java/local/example/data/ApplicationTests.java
@@ -19,12 +19,26 @@
 package local.example.data;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void someTest() {
+	void existsTest() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("http://127.0.0.1:8080"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("_links").exists());
 	}
 }
