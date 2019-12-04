d07eb297b3d95a5f7c13ee829be0ca0a3745139d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-04 07:18:51 +0100

    Update Book.java

--- library-database/src/main/java/local/example/data/model/Book.java
+++ library-database/src/main/java/local/example/data/model/Book.java
@@ -21,12 +21,10 @@ package local.example.data.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "book")
 public class Book {
 
     @Id
@@ -36,5 +34,6 @@ public class Book {
 
     @Getter
     @Setter
+    @Column(nullable = false)
     private String title;
 }
