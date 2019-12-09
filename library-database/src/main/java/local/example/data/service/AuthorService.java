9498f9b93b03cc00533cbc3b991f5438a0891575
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-09 07:47:41 +0100

    Create AuthorService.java

---
+++ library-database/src/main/java/local/example/data/service/AuthorService.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.service;
+
+import local.example.data.model.Author;
+import local.example.data.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public Collection<Author> selectAll() {
+        return authorRepository.selectAll();
+    }
+
+    public Collection<Author> selectAllOrderByLastName() {
+        return authorRepository.selectAllOrderByLastName();
+    }
+
+    public Optional<Author> selectWhereId(Long id) {
+        return authorRepository.selectWhereId(id);
+    }
+
+    public Collection<Author> selectLikeFirstName(String firstName) {
+        return authorRepository.selectLikeFirstName(firstName);
+    }
+
+    public Collection<Author> selectWhereLastName(@NotNull String lastName) {
+        return authorRepository.selectWhereLastName(lastName);
+    }
+}
