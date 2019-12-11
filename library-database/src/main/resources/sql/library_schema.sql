7a39e88c23e8b2b32665380489ab3225b7755aaf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-11 23:28:33 +0100

    Update library_schema.sql

--- library-database/src/main/resources/sql/library_schema.sql
+++ library-database/src/main/resources/sql/library_schema.sql
@@ -18,3 +18,18 @@ CREATE TABLE public.author_book (
 	CONSTRAINT author_book_fk FOREIGN KEY (author_id) REFERENCES public.author(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT author_book_fk_1 FOREIGN KEY (book_id) REFERENCES public.book(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE public.reader (
+	id bigint NOT NULL,
+	first_name varchar(30) NULL,
+	last_name varchar(30) NOT NULL,
+	CONSTRAINT reader_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.reader_book (
+	reader_id bigint NOT NULL,
+	book_id int NOT NULL,
+	CONSTRAINT reader_book_pk PRIMARY KEY (reader_id,book_id),
+	CONSTRAINT reader_book_fk FOREIGN KEY (reader_id) REFERENCES public.reader(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT reader_book_fk_1 FOREIGN KEY (book_id) REFERENCES public.book(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
