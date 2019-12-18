c3a74f2d338e98e27c0c2d2f81348cfe82a959cd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-18 18:52:20 +0100

    Create Room.java

---
+++ hotel-management/src/main/java/local/example/data/domain/Room.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@SuppressWarnings("unused")
+public class Room {
+	private Long id;
+	private Integer beds;
+	private Float basePrice;
+	private Float bathroom;
+	private Float frigobar;
+	private Float coolingFan;
+	private Float AirConditioning;
+	private Float laundry;
+	private Float shoemaker;
+	private Float catering;
+	private Float wifi;
+	private Float gigabitEthernet;
+	private Float privateBalcony;
+}
