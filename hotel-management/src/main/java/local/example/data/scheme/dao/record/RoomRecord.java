ce7466cbb1e9c3b7483005b8c4e80f1954e2a5ef
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-21 17:09:23 +0100

    Update RoomRecord.java

--- hotel-management/src/main/java/local/example/data/scheme/dao/record/RoomRecord.java
+++ hotel-management/src/main/java/local/example/data/scheme/dao/record/RoomRecord.java
@@ -18,6 +18,366 @@
 
 package local.example.data.scheme.dao.record;
 
-public class RoomRecord {
+import org.jooq.Field;
+import org.jooq.Record13;
+import org.jooq.Row13;
+import org.jooq.Table;
+import org.jooq.impl.UpdatableRecordImpl;
+import org.jooq.types.ULong;
 
+public class RoomRecord 
+	extends UpdatableRecordImpl<RoomRecord> 
+	implements Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> {
+
+	private static final long serialVersionUID = -9142412866049967992L;
+
+	public RoomRecord(Table<RoomRecord> table) {
+		super(table);
+		// TODO
+	}
+
+	@Override
+	public Row13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> fieldsRow() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Row13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> valuesRow() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<ULong> field1() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Integer> field2() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field3() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field4() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field5() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field6() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field7() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field8() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field9() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field10() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field11() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field12() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Double> field13() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public ULong value1() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Integer value2() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value3() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value4() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value5() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value6() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value7() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value8() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value9() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value10() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value11() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value12() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double value13() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value1(
+			ULong value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value2(
+			Integer value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value3(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value4(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value5(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value6(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value7(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value8(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value9(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value10(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value11(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value12(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> value13(
+			Double value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record13<ULong, Integer, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double, Double> values(
+			ULong t1, Integer t2, Double t3, Double t4, Double t5, Double t6, Double t7, Double t8, Double t9,
+			Double t10, Double t11, Double t12, Double t13) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public ULong component1() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Integer component2() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component3() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component4() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component5() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component6() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component7() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component8() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component9() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component10() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component11() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component12() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Double component13() {
+		// TODO
+		return null;
+	}
 }
