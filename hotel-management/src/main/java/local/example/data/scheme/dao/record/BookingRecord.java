8892545acd59f671a0b39086f864ef852004fb3a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-21 17:10:09 +0100

    Update BookingRecord.java

--- hotel-management/src/main/java/local/example/data/scheme/dao/record/BookingRecord.java
+++ hotel-management/src/main/java/local/example/data/scheme/dao/record/BookingRecord.java
@@ -18,6 +18,137 @@
 
 package local.example.data.scheme.dao.record;
 
-public class BookingRecord {
+import java.sql.Date;
 
+import org.jooq.Field;
+import org.jooq.Record4;
+import org.jooq.Row4;
+import org.jooq.Table;
+import org.jooq.impl.UpdatableRecordImpl;
+import org.jooq.types.ULong;
+
+public class BookingRecord 
+	extends UpdatableRecordImpl<BookingRecord> 
+	implements Record4<ULong, String, Date, Date> {
+
+	private static final long serialVersionUID = -5016972913785784538L;
+
+	public BookingRecord(Table<BookingRecord> table) {
+		super(table);
+		// TODO
+	}
+
+	@Override
+	public Row4<ULong, String, Date, Date> fieldsRow() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Row4<ULong, String, Date, Date> valuesRow() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<ULong> field1() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<String> field2() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Date> field3() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Field<Date> field4() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public ULong value1() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public String value2() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Date value3() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Date value4() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record4<ULong, String, Date, Date> value1(ULong value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record4<ULong, String, Date, Date> value2(String value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record4<ULong, String, Date, Date> value3(Date value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record4<ULong, String, Date, Date> value4(Date value) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Record4<ULong, String, Date, Date> values(ULong t1, String t2, Date t3, Date t4) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public ULong component1() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public String component2() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Date component3() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Date component4() {
+		// TODO
+		return null;
+	}
 }
