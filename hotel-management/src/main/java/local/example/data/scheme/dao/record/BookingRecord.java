a505e19f0d35dac5f2c3cbc62277b08c0dd3a118
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-24 23:48:54 +0100

    Update BookingRecord.java

--- hotel-management/src/main/java/local/example/data/scheme/dao/record/BookingRecord.java
+++ hotel-management/src/main/java/local/example/data/scheme/dao/record/BookingRecord.java
@@ -21,11 +21,13 @@ package local.example.data.scheme.dao.record;
 import java.sql.Date;
 
 import org.jooq.Field;
+import org.jooq.Record1;
 import org.jooq.Record4;
 import org.jooq.Row4;
-import org.jooq.Table;
 import org.jooq.impl.UpdatableRecordImpl;
 import org.jooq.types.ULong;
+
+import local.example.data.scheme.dao.BookingDao;
 
 public class BookingRecord 
 	extends UpdatableRecordImpl<BookingRecord> 
@@ -34,108 +36,136 @@ public class BookingRecord
 	private static final long serialVersionUID = -5016972913785784538L;
 	
 	/* constructors */
-
-	public BookingRecord(Table<BookingRecord> table) {
-		super(table);
-		// TODO
+	
+	public BookingRecord() {
+		super(BookingDao.BOOKING_DAO);
+	}
+	
+	public BookingRecord(ULong id, String vacancy, Date checkIn, Date checkOut) {
+		super(BookingDao.BOOKING_DAO);
+		super.set(1, vacancy);
+		super.set(2, checkIn);
+		super.set(3, checkOut);
 	}
 	
 	/* getter */
 	
+	public ULong getId() {
+		return (ULong) super.get(0);
+	}
+	
+	public String getVacancy() {
+		return (String) super.get(1);
+	}
+	
+	public Date getCheckIn() {
+		return (Date) super.get(2);
+	}
+	
+	public Date getCheckOut() {
+		return (Date) super.get(3);
+	}
+	
 	/* setter */
+	
+	public void setVacancy(String vacancy) {
+		super.set(1, vacancy);
+	}
+	
+	public void setCheckIn(Date checkIn) {
+		super.set(2, checkIn);
+	}
+	
+	public void setCheckOut(Date checkOut) {
+		super.set(3, checkOut);
+	}
 	
 	/* update */
 	
 	/* overrides */
-
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public Row4<ULong, String, Date, Date> fieldsRow() {
-		// TODO
-		return null;
+	public Record1<ULong> key() {
+		return (Record1) super.key();
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public Row4<ULong, String, Date, Date> fieldsRow() {
+		return (Row4) super.fieldsRow();
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public Row4<ULong, String, Date, Date> valuesRow() {
-		// TODO
-		return null;
+		return (Row4) super.valuesRow();
 	}
 	
 	/* override fields */
 
 	@Override
 	public Field<ULong> field1() {
-		// TODO
-		return null;
+		return BookingDao.BOOKING_DAO.ID;
 	}
 
 	@Override
 	public Field<String> field2() {
-		// TODO
-		return null;
+		return BookingDao.BOOKING_DAO.VACANCY;
 	}
 
 	@Override
 	public Field<Date> field3() {
-		// TODO
-		return null;
+		return BookingDao.BOOKING_DAO.CHECK_IN;
 	}
 
 	@Override
 	public Field<Date> field4() {
-		// TODO
-		return null;
+		return BookingDao.BOOKING_DAO.CHECK_OUT;
 	}
 	
 	/* override values */
 
 	@Override
 	public ULong value1() {
-		// TODO
-		return null;
+		return this.getId();
 	}
 
 	@Override
 	public String value2() {
-		// TODO
-		return null;
+		return this.getVacancy();
 	}
 
 	@Override
 	public Date value3() {
-		// TODO
-		return null;
+		return this.getCheckIn();
 	}
 
 	@Override
 	public Date value4() {
-		// TODO
-		return null;
+		return this.getCheckOut();
 	}
 	
 	/* override components */
 
 	@Override
 	public ULong component1() {
-		// TODO
-		return null;
+		return this.getId();
 	}
 
 	@Override
 	public String component2() {
-		// TODO
-		return null;
+		return this.getVacancy();
 	}
 
 	@Override
 	public Date component3() {
-		// TODO
-		return null;
+		return this.getCheckIn();
 	}
 
 	@Override
 	public Date component4() {
-		// TODO
-		return null;
+		return this.getCheckOut();
 	}
 	
 	/* other overrides */
