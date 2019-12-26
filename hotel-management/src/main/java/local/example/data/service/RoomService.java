42a2dee7cccf930ff1ffdadfbe7e0738029d325b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-12-26 02:13:36 +0100

    Update RoomService.java

--- hotel-management/src/main/java/local/example/data/service/RoomService.java
+++ hotel-management/src/main/java/local/example/data/service/RoomService.java
@@ -18,7 +18,7 @@
 
 package local.example.data.service;
 
-//import static local.example.data.scheme.dao.RoomDao.ROOM_DAO;
+import static local.example.data.scheme.dao.RoomDao.ROOM_DAO;
 
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +26,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import local.example.data.domain.Room;
-import local.example.data.scheme.dao.RoomDao;
 import local.example.data.scheme.dao.record.RoomRecord;
 
 @Service
@@ -36,10 +35,22 @@ public class RoomService {
 	@Autowired
 	private DSLContext dslContext;
 	
-	public RoomDao createRoom(Room room) {
-		String queries = "";
-		@SuppressWarnings("unused")
-		RoomRecord roomRecord = (RoomRecord) dslContext.batch(queries);
-		return null;
+	public Room createRoom(Room room) {
+		RoomRecord roomRecord = (RoomRecord) dslContext
+				.insertInto(ROOM_DAO)
+				.set(ROOM_DAO.BEDS, room.getBeds())
+				.set(ROOM_DAO.BASE_PRICE, room.getBasePrice())
+				.set(ROOM_DAO.BATHROOM, room.getBathroom())
+				.set(ROOM_DAO.FRIGOBAR, room.getFrigobar())
+				.set(ROOM_DAO.COOLING_FAN, room.getCoolingFan())
+				.set(ROOM_DAO.AIR_CONDITIONING, room.getAirConditioning())
+				.set(ROOM_DAO.LAUNDRY, room.getLaundry())
+				.set(ROOM_DAO.SHOEMAKER, room.getShoemaker())
+				.set(ROOM_DAO.CATERING, room.getCatering())
+				.set(ROOM_DAO.WIFI, room.getWifi())
+				.set(ROOM_DAO.GIGABIT_ETHERNET, room.getGigabitEthernet())
+				.set(ROOM_DAO.PRIVATE_BALCONY, room.getPrivateBalcony());
+		room.setId(roomRecord.component1());
+		return room;
 	}
 }
