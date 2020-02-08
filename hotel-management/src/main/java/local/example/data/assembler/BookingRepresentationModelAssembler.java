21fbfa932f77da74be184533292f956dca68b502
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-08 08:46:04 +0100

    Create BookingRepresentationModelAssembler.java

---
+++ hotel-management/src/main/java/local/example/data/assembler/BookingRepresentationModelAssembler.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.assembler;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import local.example.data.domain.Booking;
+
+@Component
+public class BookingRepresentationModelAssembler 
+		implements RepresentationModelAssembler<Booking, EntityModel<Booking>> {
+
+	@Override
+	public EntityModel<Booking> toModel(Booking booking) {
+		// TODO
+		return null;
+	}
+}
