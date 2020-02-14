58ec4443f38cbf43ac8e81a1028258b6c9ba90a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-14 11:07:24 +0100

    Update ProducerRepository.java

--- piece-management/src/main/java/local/example/data/repository/ProducerRepository.java
+++ piece-management/src/main/java/local/example/data/repository/ProducerRepository.java
@@ -18,9 +18,16 @@
 
 package local.example.data.repository;
 
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
-public interface ProducerRepository {
+import local.example.data.entity.Producer;
 
+@RepositoryRestResource
+public interface ProducerRepository 
+		extends PagingAndSortingRepository<Producer, Long> {
+	List<Producer> findByNickname(@Param("nickname") String nickname);
 }
