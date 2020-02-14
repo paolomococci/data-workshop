c750e339249b611933cf7dbef311ea4e13965d43
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-14 11:04:21 +0100

    Update DesignerRepository.java

--- piece-management/src/main/java/local/example/data/repository/DesignerRepository.java
+++ piece-management/src/main/java/local/example/data/repository/DesignerRepository.java
@@ -18,9 +18,16 @@
 
 package local.example.data.repository;
 
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
-public interface DesignerRepository {
+import local.example.data.entity.Designer;
 
+@RepositoryRestResource
+public interface DesignerRepository 
+		extends PagingAndSortingRepository<Designer, Long> {
+	List<Designer> findByNickname(@Param("nickname") String nickname);
 }
