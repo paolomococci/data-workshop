83e89ec9b83f9885859333d922425aae301273c9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-17 07:58:00 +0100

    Update PieceRestController.java

--- piece-management/src/main/java/local/example/data/rest/controller/PieceRestController.java
+++ piece-management/src/main/java/local/example/data/rest/controller/PieceRestController.java
@@ -22,11 +22,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import local.example.data.assembler.PieceRepresentationModelAssembler;
+import local.example.data.entity.Piece;
 import local.example.data.repository.PieceRepository;
 
 @RepositoryRestController
@@ -39,6 +44,11 @@ public class PieceRestController {
 	@Autowired
 	PieceRepresentationModelAssembler pieceRepresentationModelAssembler;
 	
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Piece piece) {
+		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+	}
+	
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<?> read(@PathVariable Long id) {
 		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
@@ -46,6 +56,18 @@ public class PieceRestController {
 	
 	@GetMapping
 	public ResponseEntity<?> readAll() {
+		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+	}
+	
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<?> update(
+			@RequestBody Piece piece, 
+			@PathVariable Long id) {
+		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
 		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
 	}
 }
