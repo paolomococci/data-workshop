c4939aa8ce228abceb185d8a345f742d096f9649
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-17 06:45:11 +0100

    Update DesignerNotFoundAdvice.java

--- piece-management/src/main/java/local/example/data/advice/DesignerNotFoundAdvice.java
+++ piece-management/src/main/java/local/example/data/advice/DesignerNotFoundAdvice.java
@@ -18,9 +18,21 @@
 
 package local.example.data.advice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import local.example.data.exception.DesignerNotFoundException;
 
 @ControllerAdvice
 public class DesignerNotFoundAdvice {
 
+	@ResponseBody
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String designerNotFoundHandler(DesignerNotFoundException designerNotFoundException) {
+		return designerNotFoundException.getMessage();
+	}
 }
