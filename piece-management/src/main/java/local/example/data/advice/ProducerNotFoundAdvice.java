c176afdd6f74f6dc9fb29e7c4250f1bfa93f9dec
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-17 06:42:31 +0100

    Update ProducerNotFoundAdvice.java

--- piece-management/src/main/java/local/example/data/advice/ProducerNotFoundAdvice.java
+++ piece-management/src/main/java/local/example/data/advice/ProducerNotFoundAdvice.java
@@ -18,9 +18,21 @@
 
 package local.example.data.advice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import local.example.data.exception.ProducerNotFoundException;
 
 @ControllerAdvice
 public class ProducerNotFoundAdvice {
 
+	@ResponseBody
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String producerNotFoundHandler(ProducerNotFoundException producerNotFoundException) {
+		return producerNotFoundException.getMessage();
+	}
 }
