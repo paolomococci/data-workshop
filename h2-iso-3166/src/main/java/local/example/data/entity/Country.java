cee7a5de4c9bcaddb428fa4d23322773d0fc1f30
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-25 07:20:49 +0100

    Update Country.java

--- h2-iso-3166/src/main/java/local/example/data/entity/Country.java
+++ h2-iso-3166/src/main/java/local/example/data/entity/Country.java
@@ -18,6 +18,38 @@
 
 package local.example.data.entity;
 
-public class Country {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Country {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	private long id;
+
+	@Getter
+	@Setter
+	@NotNull
+	private String name;
+
+	@Getter
+	@Setter
+	private String alphaTwo;
+
+	@Getter
+	@Setter
+	private String alphaThree;
+
+	@Getter
+	@Setter
+	@NotNull
+	private Integer countryId;
 }
