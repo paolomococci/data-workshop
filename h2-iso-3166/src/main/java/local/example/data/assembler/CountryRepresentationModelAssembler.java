3516b8ccc14a4dddf5784a61f2723d77b7ef2ffe
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-26 09:51:44 +0100

    Update CountryRepresentationModelAssembler.java

--- h2-iso-3166/src/main/java/local/example/data/assembler/CountryRepresentationModelAssembler.java
+++ h2-iso-3166/src/main/java/local/example/data/assembler/CountryRepresentationModelAssembler.java
@@ -18,26 +18,41 @@
 
 package local.example.data.assembler;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URISyntaxException;
+
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 import local.example.data.entity.Country;
+import local.example.data.rest.controller.CountryRestController;
 
 @Component
 public class CountryRepresentationModelAssembler 
 		implements RepresentationModelAssembler<Country, EntityModel<Country>> {
 
 	@Override
-	public EntityModel<Country> toModel(Country entity) {
-		// TODO
-		return null;
+	public EntityModel<Country> toModel(Country country) {
+		try {
+			return new EntityModel<>(country, 
+					linkTo(methodOn(CountryRestController.class).read(country.getId())).withSelfRel(), 
+					linkTo(methodOn(CountryRestController.class).readAll()).withRel("countries"));
+		} catch (URISyntaxException uriException) {
+			uriException.printStackTrace();
+		}
+		return new EntityModel<>(new Country());
 	}
 
 	@Override
-	public CollectionModel<EntityModel<Country>> toCollectionModel(Iterable<? extends Country> countries) {
-		// TODO
-		return RepresentationModelAssembler.super.toCollectionModel(countries);
+	public CollectionModel<EntityModel<Country>> toCollectionModel(
+			Iterable<? extends Country> countries
+			) {
+		return RepresentationModelAssembler
+				.super
+				.toCollectionModel(countries);
 	}
 }
