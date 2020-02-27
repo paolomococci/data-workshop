88607952ff42ea45b1965f0b349c6105edf711fe
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-27 10:19:33 +0100

    Update CountryRepositoryMockMvcTests.java

--- h2-iso-3166/src/test/java/local/example/data/repository/CountryRepositoryMockMvcTests.java
+++ h2-iso-3166/src/test/java/local/example/data/repository/CountryRepositoryMockMvcTests.java
@@ -18,6 +18,22 @@
 
 package local.example.data.repository;
 
-public class CountryRepositoryMockMvcTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CountryRepositoryMockMvcTests {
+	
+	@Autowired
+	CountryRestRepository countryRepository;
+	
+	@Test
+	public void contextTest() 
+			throws Exception {
+		assertThat(countryRepository)
+			.isNotNull();
+	}
 }
