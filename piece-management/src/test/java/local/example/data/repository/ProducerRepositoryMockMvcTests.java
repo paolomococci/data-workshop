2b84c02fff4156c04a9277d7128cbe5bb85b38e6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-28 08:17:58 +0100

    Update ProducerRepositoryMockMvcTests.java

--- piece-management/src/test/java/local/example/data/repository/ProducerRepositoryMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/repository/ProducerRepositoryMockMvcTests.java
@@ -18,6 +18,22 @@
 
 package local.example.data.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ProducerRepositoryMockMvcTests {
 
+	@Autowired
+	ProducerRepository producerRepository;
+
+	@Test
+	public void contextTest() 
+			throws Exception {
+		assertThat(producerRepository)
+			.isNotNull();
+	}
 }
