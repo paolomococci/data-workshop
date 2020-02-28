a5613c8d606bddd6bfe6fdf6a7bf4bbdd7c6ae87
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-28 08:31:35 +0100

    Update ProducerRestControllerMockMvcTests.java

--- piece-management/src/test/java/local/example/data/rest/controller/ProducerRestControllerMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/rest/controller/ProducerRestControllerMockMvcTests.java
@@ -18,6 +18,40 @@
 
 package local.example.data.rest.controller;
 
-public class ProducerRestControllerMockMvcTests {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProducerRestControllerMockMvcTests {
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	@Autowired
+	ProducerRestController producerRestController;
+
+	@Test
+	public void contextTest() 
+			throws Exception {
+		assertThat(producerRestController)
+			.isNotNull();
+	}
+
+	@Test
+	void existenceTest() 
+			throws Exception {
+		mockMvc
+			.perform(get("/api/producers"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
 }
