6a151beafe75a28a4135bc59a7a708fd38bb7f54
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-28 08:32:40 +0100

    Update PlayerRestControllerMockMvcTests.java

--- piece-management/src/test/java/local/example/data/rest/controller/PlayerRestControllerMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/rest/controller/PlayerRestControllerMockMvcTests.java
@@ -18,6 +18,40 @@
 
 package local.example.data.rest.controller;
 
-public class PlayerRestControllerMockMvcTests {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PlayerRestControllerMockMvcTests {
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	@Autowired
+	PlayerRestController playerRestController;
+
+	@Test
+	public void contextTest() 
+			throws Exception {
+		assertThat(playerRestController)
+			.isNotNull();
+	}
+
+	@Test
+	void existenceTest() 
+			throws Exception {
+		mockMvc
+			.perform(get("/api/players"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
 }
