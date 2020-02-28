4191336b031c6776afbb4c0343ac5112c4db2363
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-28 08:34:00 +0100

    Update DirectorRestControllerMockMvcTests.java

--- piece-management/src/test/java/local/example/data/rest/controller/DirectorRestControllerMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/rest/controller/DirectorRestControllerMockMvcTests.java
@@ -18,6 +18,40 @@
 
 package local.example.data.rest.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DirectorRestControllerMockMvcTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	DirectorRestController directorResrController;
+
+	@Test
+	public void contextTest() 
+			throws Exception {
+		assertThat(directorResrController)
+			.isNotNull();
+	}
+
+	@Test
+	void existenceTest() 
+			throws Exception {
+		mockMvc
+			.perform(get("/api/directors"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
 }
