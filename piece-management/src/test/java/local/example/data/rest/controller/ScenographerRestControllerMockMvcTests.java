c33bf5cee330b32c0a28072b450de2186c1de618
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-28 08:31:06 +0100

    Update ScenographerRestControllerMockMvcTests.java

--- piece-management/src/test/java/local/example/data/rest/controller/ScenographerRestControllerMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/rest/controller/ScenographerRestControllerMockMvcTests.java
@@ -18,6 +18,40 @@
 
 package local.example.data.rest.controller;
 
-public class ScenographerRestControllerMockMvcTests {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ScenographerRestControllerMockMvcTests {
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	@Autowired
+	ScenographerRestController scenographerRestController;
+
+	@Test
+	public void contextTest() 
+			throws Exception {
+		assertThat(scenographerRestController)
+			.isNotNull();
+	}
+
+	@Test
+	void existenceTest() 
+			throws Exception {
+		mockMvc
+			.perform(get("/api/scenographers"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
 }
