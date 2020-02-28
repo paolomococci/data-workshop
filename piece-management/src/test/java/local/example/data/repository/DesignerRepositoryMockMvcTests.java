78ecddd4eb29259b976ee618f628ff8d1bc5837b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-28 08:03:15 +0100

    Update DesignerRepositoryMockMvcTests.java

--- piece-management/src/test/java/local/example/data/repository/DesignerRepositoryMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/repository/DesignerRepositoryMockMvcTests.java
@@ -18,6 +18,22 @@
 
 package local.example.data.repository;
 
-public class DesignerRepositoryMockMvcTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DesignerRepositoryMockMvcTests {
+	
+	@Autowired
+	DesignerRepository designerRepository;
+	
+	@Test
+	public void contextTest() 
+			throws Exception {
+		assertThat(designerRepository)
+			.isNotNull();
+	}
 }
