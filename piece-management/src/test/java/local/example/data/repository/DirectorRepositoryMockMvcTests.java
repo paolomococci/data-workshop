535013ee277b65fcd720b11339e0caf5c0c44e2c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-02-29 22:33:53 +0100

    Update DirectorRepositoryMockMvcTests.java

--- piece-management/src/test/java/local/example/data/repository/DirectorRepositoryMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/repository/DirectorRepositoryMockMvcTests.java
@@ -36,4 +36,34 @@ public class DirectorRepositoryMockMvcTests {
 		assertThat(directorRepository)
 			.isNotNull();
 	}
+
+	@Test
+	public void createTest() 
+			throws Exception {
+		
+	}
+
+	@Test
+	public void readTest() 
+			throws Exception {
+		
+	}
+
+	@Test
+	public void readAllTest() 
+			throws Exception {
+		
+	}
+
+	@Test
+	public void updateTest() 
+			throws Exception {
+		
+	}
+
+	@Test
+	public void deleteTest() 
+			throws Exception {
+		
+	}
 }
