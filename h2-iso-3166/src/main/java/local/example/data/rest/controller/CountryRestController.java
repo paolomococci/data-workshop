0aef07cf628e2b76d6be2d3e58dfd1d393789f8e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-07 17:06:22 +0100

    Update CountryRestController.java

--- h2-iso-3166/src/main/java/local/example/data/rest/controller/CountryRestController.java
+++ h2-iso-3166/src/main/java/local/example/data/rest/controller/CountryRestController.java
@@ -28,6 +28,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -129,6 +130,35 @@ public class CountryRestController {
 					country.setAlphaTwo(countryUpdated.getAlphaTwo());
 					country.setAlphaThree(countryUpdated.getAlphaThree());
 					country.setCountryId(countryUpdated.getCountryId());
+					return countryRestRepository.save(country);
+				}).orElseGet(() -> {
+					return countryRestRepository.save(countryUpdated);
+				});
+		EntityModel<Country> entityModelOfCountry;
+		entityModelOfCountry = countryRepresentationModelAssembler
+				.toModel(temporaryEntityOfCountry);
+		return new ResponseEntity<>(entityModelOfCountry, HttpStatus.OK);
+	}
+	
+	@PatchMapping(path = "/{id}")
+	public ResponseEntity<?> patchUpdate(
+			@RequestBody Country countryUpdated, 
+			@PathVariable Long id) 
+			throws URISyntaxException {
+		var temporaryEntityOfCountry = countryRestRepository.findById(id)
+				.map(country -> {
+					if (countryUpdated.getName() != null) {
+						country.setName(countryUpdated.getName());
+					}
+					if (countryUpdated.getAlphaTwo() != null) {
+						country.setAlphaTwo(countryUpdated.getAlphaTwo());
+					}
+					if (countryUpdated.getAlphaThree() != null) {
+						country.setAlphaThree(countryUpdated.getAlphaThree());
+					}
+					if (countryUpdated.getCountryId() != null) {
+						country.setCountryId(countryUpdated.getCountryId());
+					}
 					return countryRestRepository.save(country);
 				}).orElseGet(() -> {
 					return countryRestRepository.save(countryUpdated);
