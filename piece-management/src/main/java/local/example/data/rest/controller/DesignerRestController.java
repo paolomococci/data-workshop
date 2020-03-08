1fb3f8ef889f3d35dde81853ca6e61690300f351
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-08 23:15:24 +0100

    Update DesignerRestController.java

--- piece-management/src/main/java/local/example/data/rest/controller/DesignerRestController.java
+++ piece-management/src/main/java/local/example/data/rest/controller/DesignerRestController.java
@@ -54,7 +54,7 @@ public class DesignerRestController {
 			throws URISyntaxException {
 		EntityModel<Designer> entityModelOfDesigner;
 		entityModelOfDesigner = designerRepresentationModelAssembler
-				.toModel(designer);
+				.toModel(designerRepository.save(designer));
 		return new ResponseEntity<>(entityModelOfDesigner, HttpStatus.CREATED);
 	}
 	
