6bcff528eb688b160a7ce5477b802a6fa9b8a16a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-03-14 16:34:14 +0100

    Update DirectorRepositoryMockMvcTests.java

--- piece-management/src/test/java/local/example/data/repository/DirectorRepositoryMockMvcTests.java
+++ piece-management/src/test/java/local/example/data/repository/DirectorRepositoryMockMvcTests.java
@@ -32,6 +32,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 public class DirectorRepositoryMockMvcTests {
@@ -87,7 +88,13 @@ public class DirectorRepositoryMockMvcTests {
 	@Test
 	public void readAllTest() 
 			throws Exception {
-		// TODO
+		MvcResult mvcResult = mockMvc
+				.perform(post("/directors").content(DIRECTOR_TEST_STRING))
+				.andExpect(status().isCreated())
+				.andReturn();
+		String result = mvcResult.getResponse().getHeader("Location");
+		mockMvc.perform(get(result))
+				.andExpect(status().isOk());
 	}
 
 	@Test
