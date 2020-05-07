9407df196c3f06b4ab2dc095368e0f4e4db1e388
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-07 22:34:55 +0200

    Create WebSecurityConfig.java

---
+++ data-flow/app-viewer/src/main/java/local/example/data/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig 
+		extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) 
+			throws Exception {
+		httpSecurity.authorizeRequests()
+			.antMatchers("/", "/home").permitAll().anyRequest().authenticated()
+			.and().formLogin().loginPage("/login").permitAll()
+			.and().logout().permitAll();
+	}
+}
