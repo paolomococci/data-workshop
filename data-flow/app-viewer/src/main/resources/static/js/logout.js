f029d05f61ecd32f402530ff727fbe034896eefd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-10 19:54:55 +0200

    Update logout.js

--- data-flow/app-viewer/src/main/resources/static/js/logout.js
+++ data-flow/app-viewer/src/main/resources/static/js/logout.js
@@ -1,1 +1,24 @@
 'use strict';
+
+class Logout extends React.Component {
+	render() {
+		return React.createElement(
+					"fieldset", 
+					null, 
+					React.createElement(
+						"legend", 
+						null, 
+						"click for logout"
+					), 
+					React.createElement(
+						"input", 
+						{ type: "submit", value: "log out" }
+					)
+				);
+	}
+}
+
+ReactDOM.render(
+		React.createElement(Logout, null), 
+		document.getElementById('logout')
+);
