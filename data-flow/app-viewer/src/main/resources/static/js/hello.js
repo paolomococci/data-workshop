a5252fee015ce55ca18b8f89550939226f5b38cd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-10 18:52:46 +0200

    Update hello.js

--- data-flow/app-viewer/src/main/resources/static/js/hello.js
+++ data-flow/app-viewer/src/main/resources/static/js/hello.js
@@ -1,3 +1,17 @@
 'use strict';
 
-const elementLogOut = React.createElement;
+class Hello extends React.Component {
+	render() {
+		return React.createElement(
+				"h1", 
+				null, 
+				"Hello ", 
+				this.props.name
+		);
+	}
+}
+
+ReactDOM.render(
+		React.createElement(Hello, {name: "World"}), 
+		document.getElementById('hello')
+);
