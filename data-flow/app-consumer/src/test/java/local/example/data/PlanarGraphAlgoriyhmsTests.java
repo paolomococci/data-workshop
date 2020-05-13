95f4c0e09b559389aeb50c9c60c211cd927335dc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-13 18:52:38 +0200

    Create PlanarGraphAlgoriyhmsTests.java

---
+++ data-flow/app-consumer/src/test/java/local/example/data/PlanarGraphAlgoriyhmsTests.java
@@ -0,0 +1,63 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PlanarGraphAlgoriyhmsTests {
+
+	private static Graph<String, DefaultWeightedEdge> weightedGraph  = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+
+	@BeforeAll
+	static void init() 
+			throws Exception {
+		weightedGraph.addVertex("A");
+		weightedGraph.addVertex("B");
+		weightedGraph.addVertex("C");
+		weightedGraph.addVertex("D");
+		weightedGraph.addVertex("E");
+		weightedGraph.addVertex("F");
+		weightedGraph.addVertex("G");
+		weightedGraph.addVertex("G");
+		weightedGraph.addVertex("G");
+		weightedGraph.addVertex("H");
+		weightedGraph.addVertex("I");
+	}
+
+	@Test
+	@DisplayName("planar test")
+	void planarTest() 
+			throws Exception {
+		
+	}
+
+	@AfterAll
+	static void end() 
+			throws Exception {
+		System.out.println("After all execution!");
+	}
+}
