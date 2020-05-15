eea737a08987f4869506dd10d948134908154399
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-15 18:06:25 +0200

    Update LoginViewController.java

--- data-flow/app-viewer/src/main/java/local/example/data/controller/LoginViewController.java
+++ data-flow/app-viewer/src/main/java/local/example/data/controller/LoginViewController.java
@@ -22,7 +22,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class LoginViewController {
 
 	@GetMapping(value = {"/login", "/login.html"})
 	public String login() {
