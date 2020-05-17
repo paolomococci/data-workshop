390dd8d0494e7450a7ca29336078d54df9b7bb88
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-17 07:02:44 +0200

    Create MainView.java

---
+++ data-flow/app-viewer/src/main/java/local/example/data/view/MainView.java
@@ -0,0 +1,52 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.view;
+
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+import local.example.data.view.component.CustomerEditorComponent;
+import local.example.data.view.component.EmployeeEditorComponent;
+import local.example.data.view.component.JobEditorComponent;
+import local.example.data.view.component.MainViewComponent;
+import lombok.Getter;
+
+@Route(value = "/main")
+@PWA(name = "Main Page", shortName = "main")
+public class MainView {
+
+	@Getter
+	private final MainViewComponent mainViewComponent;
+
+	@Getter
+	private final CustomerEditorComponent customerEditorComponent;
+
+	@Getter
+	private final EmployeeEditorComponent employeeEditorComponent;
+
+	@Getter
+	private final JobEditorComponent jobEditorComponent;
+
+	public MainView() {
+		mainViewComponent = new MainViewComponent();
+		customerEditorComponent = new CustomerEditorComponent();
+		employeeEditorComponent = new EmployeeEditorComponent();
+		jobEditorComponent = new JobEditorComponent();
+	}
+}
