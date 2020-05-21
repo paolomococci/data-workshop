561540147413033ea7b8563bdbd9981a6ce6f3b4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-21 12:27:12 +0200

    Update CustomerViewEditor.java

--- data-ui/src/main/java/local/example/data/view/editor/CustomerViewEditor.java
+++ data-ui/src/main/java/local/example/data/view/editor/CustomerViewEditor.java
@@ -18,10 +18,23 @@
 
 package local.example.data.view.editor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
+
+import local.example.data.entity.Customer;
+import local.example.data.repository.CustomerRepository;
+import local.example.data.view.handler.CustomerChangeHandler;
+import lombok.Getter;
+import lombok.Setter;
 
 @SpringComponent
 @UIScope
@@ -30,5 +43,48 @@ public class CustomerViewEditor
 		implements KeyNotifier {
 
 	private static final long serialVersionUID = -2486816437236479000L;
+	private final CustomerRepository customerRepository;
+	private final TextField nickname;
+	private final TextField abcClassification;
+	private final Button save;
+	private final Button cancel;
+	private final Button delete;
+	private final HorizontalLayout tools;
+	private final Binder<Customer> binderOfCustomers;
+	private CustomerChangeHandler changeHandler;
 
+	@Getter
+	@Setter
+	private Customer customer;
+
+	@Autowired
+	public CustomerViewEditor(CustomerRepository customerRepository) {
+		super();
+		this.customerRepository = customerRepository;
+		this.nickname = new TextField("nickname");
+		this.abcClassification = new TextField("abc classification");
+		this.save = new Button("save", VaadinIcon.UPLOAD.create());
+		this.cancel = new Button("cancel", VaadinIcon.MINUS_CIRCLE.create());
+		this.delete = new Button("delete", VaadinIcon.TRASH.create());
+		this.tools = new HorizontalLayout(save, cancel, delete);
+		this.binderOfCustomers = new Binder<>(Customer.class);
+		this.binderOfCustomers.bindInstanceFields(this);
+		this.setSpacing(true);
+		this.add(nickname, abcClassification, tools);
+		this.setVisible(false);
+	}
+
+	public void save() {
+		// TODO
+		changeHandler.onChange();
+	}
+
+	public void cancel() {
+		// TODO
+	}
+
+	public void delete() {
+		// TODO
+		changeHandler.onChange();
+	}
 }
