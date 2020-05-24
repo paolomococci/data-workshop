8a7806bee12601399bfa56d70521f3fb97b5d91c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-24 23:52:12 +0200

    Create HelpComponent.java

---
+++ crud-update/src/main/java/local/example/data/view/component/HelpComponent.java
@@ -0,0 +1,53 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.view.component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+@SpringComponent
+@UIScope
+public class HelpComponent 
+		extends VerticalLayout {
+
+	private static final long serialVersionUID = 5051706727369834507L;
+
+	public static final String TITLE_VIEW = "Help View";
+	public static final String VIEW_NAME = "";
+	private static final String SUB_TITLE = "Create Read Update and Delete " + 
+			"data fields of system users";
+	private static final String PRESENTATION = "This CRUD Web Application.";
+	private static final String EXPLANITION =  
+			"From which it's possible to administer the registration and " + 
+			"cancellation of users in a hypotetical system.";
+	public final Label label = new Label(TITLE_VIEW);
+	public final Label subtitle = new Label(SUB_TITLE);
+	public final Label present = new Label(PRESENTATION);
+	public final Label explain = new Label(EXPLANITION);
+
+	@Autowired
+	public HelpComponent() {
+		super();
+		this.add(label, subtitle, present, explain);
+	}
+}
