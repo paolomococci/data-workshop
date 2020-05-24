67c1bf065ef98c41e00d9c9b87fe31f4d1051607
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-24 23:48:47 +0200

    Create Alpha.java

---
+++ crud-update/src/main/java/local/example/data/validator/constraits/Alpha.java
@@ -0,0 +1,42 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.validator.constraints;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import local.example.data.validator.AlphaValidator;
+
+@Documented
+@Retention(RUNTIME)
+@Target(FIELD)
+@Constraint(validatedBy = AlphaValidator.class)
+public @interface Alpha {
+
+	String message() default "local.example.data.validator.constraints.Alpha";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default{};
+}
