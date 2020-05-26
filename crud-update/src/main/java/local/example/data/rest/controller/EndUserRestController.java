8e6011c44946f1784c35175bb29b94a1e74f4450
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-26 05:24:55 +0200

    Update EndUserRestController.java

--- crud-update/src/main/java/local/example/data/rest/controller/EndUserRestController.java
+++ crud-update/src/main/java/local/example/data/rest/controller/EndUserRestController.java
@@ -18,11 +18,26 @@
 
 package local.example.data.rest.repository;
 
+import java.net.URISyntaxException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import local.example.data.assembler.EndUserRepresentationModelAssembler;
+import local.example.data.entity.EndUser;
+import local.example.data.exception.EndUserNotFoundException;
 
 @RepositoryRestController
 @RequestMapping(path = "/rest/endUsers")
@@ -33,4 +48,116 @@ public class EndUserRestController {
 
 	@Autowired
 	EndUserRepresentationModelAssembler endUserRepresentationModelAssembler;
+
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody EndUser endUser) 
+			throws URISyntaxException {
+		EntityModel<EndUser> entityModelOfEndUser;
+		entityModelOfEndUser = endUserRepresentationModelAssembler
+				.toModel(endUserRestRepository.save(endUser));
+		return new ResponseEntity<>(entityModelOfEndUser, HttpStatus.CREATED);
+	}
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<?> read(@PathVariable Long id) 
+			throws URISyntaxException {
+		EndUser endUser = endUserRestRepository.findById(id)
+				.orElseThrow(() -> new EndUserNotFoundException(id));
+		EntityModel<EndUser> entityModelOfEndUser;
+		entityModelOfEndUser = endUserRepresentationModelAssembler.toModel(endUser);
+		return new ResponseEntity<>(entityModelOfEndUser, HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/names/{name}")
+	public ResponseEntity<?> readByName(@PathVariable String name) 
+			throws URISyntaxException {
+		Iterable<EndUser> endUsers = endUserRestRepository.findByName(name);
+		CollectionModel<EntityModel<EndUser>> collectionModelOfEndUsers;
+		collectionModelOfEndUsers = endUserRepresentationModelAssembler
+				.toCollectionModel(endUsers);
+		return new ResponseEntity<>(collectionModelOfEndUsers, HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/surnames/{surname}")
+	public ResponseEntity<?> readBySurname(@PathVariable String surname) 
+			throws URISyntaxException {
+		Iterable<EndUser> endUsers = endUserRestRepository.findBySurname(surname);
+		CollectionModel<EntityModel<EndUser>> collectionModelOfEndUsers;
+		collectionModelOfEndUsers = endUserRepresentationModelAssembler
+				.toCollectionModel(endUsers);
+		return new ResponseEntity<>(collectionModelOfEndUsers, HttpStatus.OK);
+	}
+	
+	@GetMapping(path = "/usernames/{username}")
+	public ResponseEntity<?> readByUsername(@PathVariable String username) 
+			throws URISyntaxException {
+		Iterable<EndUser> endUsers = endUserRestRepository.findByUsername(username);
+		CollectionModel<EntityModel<EndUser>> collectionModelOfEndUsers;
+		collectionModelOfEndUsers = endUserRepresentationModelAssembler
+				.toCollectionModel(endUsers);
+		return new ResponseEntity<>(collectionModelOfEndUsers, HttpStatus.OK);
+	}
+
+	@GetMapping
+	public ResponseEntity<?> readAll() 
+			throws URISyntaxException {
+		Iterable<EndUser> endUsers = endUserRestRepository.findAll();
+		CollectionModel<EntityModel<EndUser>> collectionModelOfEndUsers;
+		collectionModelOfEndUsers = endUserRepresentationModelAssembler
+				.toCollectionModel(endUsers);
+		return new ResponseEntity<>(collectionModelOfEndUsers, HttpStatus.OK);
+	}
+
+	@PutMapping(path = "/{id}")
+	public ResponseEntity<?> putUpdate(
+			@RequestBody EndUser endUserUpdated, 
+			@PathVariable Long id) 
+			throws URISyntaxException {
+		 EndUser temporaryEntityOfEndUser = endUserRestRepository.findById(id)
+				.map(endUser -> {
+					endUser.setName(endUserUpdated.getName());
+					endUser.setSurname(endUserUpdated.getSurname());
+					endUser.setUsername(endUserUpdated.getUsername());
+					return endUserRestRepository.save(endUser);
+				}).orElseGet(() -> {
+					return endUserRestRepository.save(endUserUpdated);
+				});
+		EntityModel<EndUser> entityModelOfEndUser;
+		entityModelOfEndUser = endUserRepresentationModelAssembler
+				.toModel(temporaryEntityOfEndUser);
+		return new ResponseEntity<>(entityModelOfEndUser, HttpStatus.OK);
+	}
+	
+	@PatchMapping(path = "/{id}")
+	public ResponseEntity<?> patchUpdate(
+			@RequestBody EndUser endUserUpdated, 
+			@PathVariable Long id) 
+			throws URISyntaxException {
+		EndUser temporaryEntityOfEndUser = endUserRestRepository.findById(id)
+				.map(endUser -> {
+					if (endUserUpdated.getName() != null) {
+						endUser.setName(endUserUpdated.getName());
+					}
+					if (endUserUpdated.getSurname() != null) {
+						endUser.setSurname(endUserUpdated.getSurname());
+					}
+					if (endUserUpdated.getUsername() != null) {
+						endUser.setUsername(endUserUpdated.getUsername());
+					}
+					return endUserRestRepository.save(endUser);
+				}).orElseGet(() -> {
+					return endUserRestRepository.save(endUserUpdated);
+				});
+		EntityModel<EndUser> entityModelOfEndUser;
+		entityModelOfEndUser = endUserRepresentationModelAssembler
+				.toModel(temporaryEntityOfEndUser);
+		return new ResponseEntity<>(entityModelOfEndUser, HttpStatus.OK);
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) 
+			throws URISyntaxException {
+		endUserRestRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 }
