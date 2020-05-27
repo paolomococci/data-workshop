d086bed1f088d3cad822219b67a162b7783f02ac
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-27 22:08:20 +0200

    Update RootView.java

--- crud-update/src/main/java/local/example/data/view/RootView.java
+++ crud-update/src/main/java/local/example/data/view/RootView.java
@@ -20,20 +20,23 @@ package local.example.data.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabsVariant;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.PWA;
 
 import local.example.data.view.component.HelpComponent;
 import local.example.data.view.component.OverviewComponent;
 
 @Route(value = "root")
 @RouteAlias(value = "")
+@PWA(name = "crud update", shortName = "crud")
 public class RootView 
-		extends VerticalLayout {
+		extends AppLayout {
 
 	private static final long serialVersionUID = -847263394035462160L;
 
@@ -54,6 +57,7 @@ public class RootView
 		this.endUserOverviewTab.add(overviewComponent);
 		this.tabs = new Tabs(helpComponentTab, endUserOverviewTab);
 		this.tabs.addThemeVariants(TabsVariant.LUMO_CENTERED);
-		this.add(tabs);
+		this.addToNavbar(new DrawerToggle());
+		this.addToDrawer(tabs);
 	}
 }
