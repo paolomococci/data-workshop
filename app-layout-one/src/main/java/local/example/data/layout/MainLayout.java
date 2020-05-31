6d2add50b28e1737d1ec4b574fc90a0cb3d6e8a9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-05-31 20:53:39 +0200

    Update MainLayout.java

--- app-layout-one/src/main/java/local/example/data/layout/MainLayout.java
+++ app-layout-one/src/main/java/local/example/data/layout/MainLayout.java
@@ -16,12 +16,134 @@
  *
  */
 
-package local.example.data.view;
+package local.example.data.layout;
 
-import com.vaadin.flow.component.applayout.AppLayout;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-@SuppressWarnings("serial")
-public class MainView 
-		extends AppLayout {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+
+import local.example.data.layout.component.EditorComponent;
+import local.example.data.layout.component.HelpComponent;
+import local.example.data.layout.component.OverviewComponent;
+
+@Route("main")
+public class MainLayout 
+		extends VerticalLayout {
+
+	private static final long serialVersionUID = -6086472484557942526L;
+
+	private HelpComponent helpComponent;
+	private OverviewComponent overviewComponent;
+	private EditorComponent editorComponent;
+
+	private Dialog dialog;
+	private Tab tab0, tab1, tab2;
+	private Div div0, div1, div2;
+	private Map<Integer, Component> workspace;
+	private Tabs tabs;
+	private Div contents;
+	private Set<Component> contentsShow;
+	private Button backButton;
+	private Button forwardButton;
+
+	@Autowired
+	public MainLayout() {
+		super();
+		
+		this.helpComponent = new HelpComponent();
+		this.overviewComponent = new OverviewComponent();
+		this.editorComponent = new EditorComponent();
+		
+		this.dialog = new Dialog();
+		
+		this.tab0 = new Tab("help");
+		this.div0 = new Div();
+		this.div0.add(this.helpComponent);
+		
+		this.tab1 = new Tab("overview");
+		this.div1 = new Div();
+		this.div1.add(this.overviewComponent);
+		this.div1.setVisible(false);
+		
+		this.tab2 = new Tab("editor");
+		this.div2 = new Div();
+		this.div2.add(this.editorComponent);
+		this.div2.setVisible(false);
+		
+		this.workspace = new HashMap<>();
+		this.workspace.put(0, this.div0);
+		this.workspace.put(1, this.div1);
+		this.workspace.put(2, this.div2);
+		
+		this.tabs = new Tabs(this.tab0, this.tab1, this.tab2);
+		
+		this.contents = new Div(this.div0, this.div1, this.div2);
+		
+		this.contentsShow = Stream.of(this.div0).collect(Collectors.toSet());
+		
+		this.tabs.addSelectedChangeListener(listener -> {
+			this.contentsShow.forEach(div -> div.setVisible(false));
+			this.contentsShow.clear();
+			Component selected = this.workspace.get(this.tabs.getSelectedIndex());
+			selected.setVisible(true);
+			this.contentsShow.add(selected);
+		});
+		
+		this.backButton = new Button();
+		this.backButton.setIcon(VaadinIcon.ARROW_BACKWARD.create());
+		this.backButton.setText("back");
+		this.backButton.addClickListener(listener -> {
+			if (this.tabs.getSelectedIndex() > 0) {
+				this.contentsShow.forEach(div -> div.setVisible(false));
+				this.contentsShow.clear();
+				this.tabs.setSelectedIndex(this.tabs.getSelectedIndex() - 1);
+				Component selected = this.workspace.get(this.tabs.getSelectedIndex());
+				selected.setVisible(true);
+				this.contentsShow.add(selected);
+			} else {
+				// TODO
+			}
+		});
+		
+		this.forwardButton = new Button();
+		this.forwardButton.setIcon(VaadinIcon.ARROW_FORWARD.create());
+		this.forwardButton.setText("forward");
+		this.forwardButton.addClickListener(listener -> {
+			if (this.tabs.getSelectedIndex() < (this.workspace.size() - 1)) {
+				this.contentsShow.forEach(div -> div.setVisible(false));
+				this.contentsShow.clear();
+				this.tabs.setSelectedIndex(this.tabs.getSelectedIndex() + 1);
+				Component selected = this.workspace.get(this.tabs.getSelectedIndex());
+				selected.setVisible(true);
+				this.contentsShow.add(selected);
+			} else {
+				// TODO
+			}
+		});
+		
+		this.dialog.add(
+				this.backButton, 
+				this.forwardButton, 
+				this.tabs, 
+				this.contents
+		);
+		this.dialog.setOpened(true);
+		this.dialog.setHeight("800px");
+		this.dialog.setWidth("900px");
+		this.add(this.dialog);
+	}
 }
