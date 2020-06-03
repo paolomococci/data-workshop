b3cdc80174c0f1dacad1556ba41a9108f32102dc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-06-03 11:08:43 +0200

    Update WebSecurityConfig.java

--- app-layout-one/src/main/java/local/example/data/config/WebSecurityConfig.java
+++ app-layout-one/src/main/java/local/example/data/config/WebSecurityConfig.java
@@ -18,47 +18,31 @@
 
 package local.example.data.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig 
 		extends WebSecurityConfigurerAdapter {
 
-	private static final String[] patterns = {"", "signinto", "/VAADIN/**"};
-	private static final String loginPage = "signinto";
-	private static final String username = "guest";
-	private static final String rawPassword = "123qwerty456";
-	private static final String roles = "USER";
-
-	
-	@Override
-	protected void configure(HttpSecurity httpSecurity) 
-			throws Exception {
-		httpSecurity.authorizeRequests()
-			.antMatchers(patterns).permitAll().anyRequest().authenticated()
-			.and().formLogin().loginPage(loginPage).permitAll()
-			.and().logout().permitAll();
-	}
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) 
 			throws Exception {
 		authenticationManagerBuilder.inMemoryAuthentication()
-			.withUser(username)
-			.password(this.passwordEncoder().encode(rawPassword))
-			.roles(roles);
+			.withUser("julie").password("julie123").roles("USER", "ADMIN")
+			.and().withUser("john").password("john123").roles("USER");
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	@Override
+	protected void configure(HttpSecurity httpSecurity) 
+			throws Exception {
+		httpSecurity.authorizeRequests()
+			.antMatchers("/login", "/VAADIN/**").permitAll().anyRequest().authenticated()
+			.and().formLogin().loginPage("/login").permitAll()
+			.and().logout().permitAll();
 	}
 }
