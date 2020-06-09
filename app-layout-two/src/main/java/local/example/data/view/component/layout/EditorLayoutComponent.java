170260f7526291231b101ab75c05f5b467d11630
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-06-09 06:46:27 +0200

    Create EditorLayoutComponent.java

---
+++ app-layout-two/src/main/java/local/example/data/view/component/layout/EditorLayoutComponent.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.view.component.layout;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+@SpringComponent
+@UIScope
+public class EditorLayoutComponent 
+		extends VerticalLayout 
+		implements KeyNotifier {
+
+	private static final long serialVersionUID = -7581648605737698853L;
+
+	private Label label = new Label("#editor");
+
+	@Autowired
+	public EditorLayoutComponent() {
+		super();
+		this.add(label);
+	}
+}
