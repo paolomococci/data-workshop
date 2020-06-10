581d6714c92e0f205ce953753585cee84154a914
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-06-10 10:19:02 +0200

    Update JdbcHttpSessionConfig.java

--- app-layout-two/src/main/java/local/example/data/config/JdbcHttpSessionConfig.java
+++ app-layout-two/src/main/java/local/example/data/config/JdbcHttpSessionConfig.java
@@ -18,6 +18,29 @@
 
 package local.example.data.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@EnableJdbcHttpSession
 public class JdbcHttpSessionConfig {
 
+	private final String SQL_SCHEME_SCRIPT = "storedb-session-scheme.sql";
+
+	@Bean
+	public EmbeddedDatabase dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.addScript(SQL_SCHEME_SCRIPT).build();
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
 }
