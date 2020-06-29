d484d7de1bf893ec5e3594ca2bc4f67f3809f42c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-06-29 09:12:59 +0200

    Update Customer.java

--- sample-crm/src/main/main/java/local/example/data/model/Customer.java
+++ sample-crm/src/main/main/java/local/example/data/model/Customer.java
@@ -19,17 +19,20 @@
 package local.example.data.model;
 
 import java.time.LocalDate;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class Customer {
 
