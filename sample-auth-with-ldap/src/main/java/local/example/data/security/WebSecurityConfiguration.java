3999f751fcb76943b55fa82342bd2d4dde74a48a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-08 18:01:50 +0200

    Update WebSecurityConfiguration.java

--- sample-auth-with-ldap/src/main/java/local/example/data/security/WebSecurityConfiguration.java
+++ sample-auth-with-ldap/src/main/java/local/example/data/security/WebSecurityConfiguration.java
@@ -18,29 +18,58 @@
 
 package local.example.data.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class WebSecurityConfiguration 
 		extends WebSecurityConfigurerAdapter {
 
-	@SuppressWarnings("unused")
 	private static final String LOGIN_URL = "/login";
-	@SuppressWarnings("unused")
 	private static final String LOGIN_FAILURE_URL = "/login?error";
-	@SuppressWarnings("unused")
-	private static final String[] ANT_PATTERNS = { 
+	private static final String[] ANT_PATTERNS = {
 			"/VAADIN/**", 
 			"/robots.txt", 
 			"/favicon.ico", 
-			"/manifest.webmanifest", 
+			"/manifest.webmanifest",
 			"/sw.js", 
 			"/offline-page.html", 
 			"/frontend/**", 
 			"/frontend-es5/**", 
 			"/frontend-es6/**", 
-			"/webjars/**", 
+			"/webjars/**",
 			"/icons/**", 
 			"/images/**", 
 			"/styles/**", 
-			"/h2-console/**"};
+			"/h2-console/**" };
+
+	@Override
+	public void configure(WebSecurity webSecurity) 
+			throws Exception {
+		webSecurity.ignoring().antMatchers(ANT_PATTERNS);
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) 
+			throws Exception {
+		httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated()
+			.and().formLogin().loginPage(LOGIN_URL).permitAll().loginProcessingUrl(LOGIN_URL)
+				.failureUrl(LOGIN_FAILURE_URL).and().logout().logoutSuccessUrl(LOGIN_URL);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) 
+			throws Exception {
+		// TODO
+		super.configure(authenticationManagerBuilder);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
