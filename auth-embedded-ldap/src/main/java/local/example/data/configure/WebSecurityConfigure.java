dc6b9f9709d661e601b8aab812136519ab982c97
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-14 09:04:45 +0200

    Update WebSecurityConfigure.java

--- auth-embedded-ldap/src/main/java/local/example/data/configure/WebSecurityConfigure.java
+++ auth-embedded-ldap/src/main/java/local/example/data/configure/WebSecurityConfigure.java
@@ -18,6 +18,42 @@
 
 package local.example.data.configure;
 
-public class WebSecurityConfigure {
-  
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure 
+		extends WebSecurityConfigurerAdapter {
+
+	private static final String[] LDAP_CONFIG = {
+			"ou=people", 
+			"(uid={0})", 
+			"ou=groups", 
+			"(member={0})", 
+			"dc=example,dc=local", 
+			"classpath:users-allowed.ldif"
+			};
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) 
+			throws Exception {
+		authenticationManagerBuilder.ldapAuthentication()
+			.userSearchBase(LDAP_CONFIG[0])
+			.userSearchFilter(LDAP_CONFIG[1])
+			.groupSearchBase(LDAP_CONFIG[2])
+			.groupSearchFilter(LDAP_CONFIG[3])
+			.contextSource()
+			.root(LDAP_CONFIG[4])
+			.ldif(LDAP_CONFIG[5]);
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) 
+			throws Exception {
+		httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin();
+	}
 }
