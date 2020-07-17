af1db6edef10ba4de84ee3cfcf464b9fbf5cc081
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-17 22:39:25 +0200

    Update ItemDataRepresentation.java

--- reactive-data-accessing-h2-view/src/main/java/local/example/data/retrieve/representation/ItemDataRepresentation.java
+++ reactive-data-accessing-h2-view/src/main/java/local/example/data/retrieve/representation/ItemDataRepresentation.java
@@ -56,4 +56,44 @@ public class ItemDataRepresentation {
 	public void setStatus(Status status) {
 		this.status = status;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemDataRepresentation other = (ItemDataRepresentation) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (status != other.status)
+			return false;
+		return true;
+	}
 }
