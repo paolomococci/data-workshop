eb774b8e06a9f05cbac35f6b21c9fb4b5c56db04
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-20 07:54:59 +0200

    Create ItemCreateView.java

---
+++ reactive-data-accessing-h2-view/src/main/java/local/example/data/view/ItemCreateView.java
@@ -0,0 +1,35 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.view;
+
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "create", layout = MainLayout.class)
+@PageTitle(value = "create")
+public class ItemCreateView 
+		extends Main {
+
+	private static final long serialVersionUID = 702507471491940975L;
+
+	public ItemCreateView() {
+		// TODO
+	}
+}
