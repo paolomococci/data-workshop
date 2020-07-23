8f4b5a4980f7cb65a9b6f0b6cd2055d744eabdb4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-07-23 21:06:09 +0200

    Update ItemMapper.java

--- reactive-data-accessing-h2-view/src/main/java/local/example/data/retrieve/mapper/ItemMapper.java
+++ reactive-data-accessing-h2-view/src/main/java/local/example/data/retrieve/mapper/ItemMapper.java
@@ -19,10 +19,16 @@
 package local.example.data.retrieve.mapper;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -37,6 +43,40 @@ public class ItemMapper {
 		for (JsonNode jsonNode : jsonNodes) {
 			items.add(objectMapper.treeToValue(jsonNode, Item.class));
 		}
+		return items;
+	}
+
+	public static Item getIdentifiedItem(URI uri, Long id) 
+			throws JsonMappingException, JsonProcessingException {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(uri+"/"+id, String.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(response.getBody());
+		Item item = new Item();
+		item.setId(id);
+		item.setCode(jsonNode.path("code").toString());
+		item.setDescription(jsonNode.path("description").toString());
+		item.setStatus(jsonNode.path("status").toString());
+		return item;
+	}
+
+	public static List<Item> getItems(URI uri) 
+			throws JsonMappingException, JsonProcessingException {
+		List<Item> items = new ArrayList<Item>();
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String[]> response = restTemplate.getForEntity(uri, String[].class);
+		List<String> entities = Arrays.asList(response.getBody());
+		ObjectMapper objectMapper = new ObjectMapper();
+		for (String entity : entities) {
+			JsonNode jsonNode = objectMapper.readTree(entity);
+			Item item = new Item(
+					Long.valueOf(jsonNode.path("id").toPrettyString()), 
+					jsonNode.path("code").toPrettyString(), 
+					jsonNode.path("description").toPrettyString(), 
+					jsonNode.path("status").toPrettyString()
+			);
+			items.add(item);
+		} 
 		return items;
 	}
 }
