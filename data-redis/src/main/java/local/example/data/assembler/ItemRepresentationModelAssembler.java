2100870de4204aad7ca929bbced7568aff4f9b6a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-14 18:53:13 +0200

    Create ItemRepresentationModelAssembler.java

---
+++ data-redis/src/main/java/local/example/data/assembler/ItemRepresentationModelAssembler.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.assembler;
+
+import local.example.data.controller.ItemRestController;
+import local.example.data.model.Item;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class ItemRepresentationModelAssembler
+        implements RepresentationModelAssembler<Item, EntityModel<Item>> {
+
+    @Override
+    public EntityModel<Item> toModel(Item item) {
+            return new EntityModel<>(item,
+                    linkTo(methodOn(ItemRestController.class).read(item.getId())).withSelfRel(),
+                    linkTo(methodOn(ItemRestController.class).readAll()).withRel("items"));
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Item>> toCollectionModel(
+            Iterable<? extends Item> items) {
+        return RepresentationModelAssembler.super.toCollectionModel(items);
+    }
+}
