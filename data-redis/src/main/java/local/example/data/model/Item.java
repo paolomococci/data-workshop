626094f322b624a864c733d745f7c0302bea633a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-14 11:46:58 +0200

    Update Item.java

--- data-redis/src/main/java/local/example/data/model/Item.java
+++ data-redis/src/main/java/local/example/data/model/Item.java
@@ -18,5 +18,17 @@
 
 package local.example.data.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
+
+    private String id;
+    private String code;
+    private String name;
+    private String description;
 }
