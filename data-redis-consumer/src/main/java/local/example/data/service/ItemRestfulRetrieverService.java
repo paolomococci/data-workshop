d8ae486625a794dc88157ff5511bdc64aef1187b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-08-19 18:00:45 +0200

    Create ItemRestfulRetrieverService.java

---
+++ data-redis-consumer/src/main/java/local/example/data/service/ItemRestfulRetrieverService.java
@@ -0,0 +1,50 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.service;
+
+import local.example.data.model.Item;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.hateoas.client.Traverson.TraversalBuilder;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class ItemRestfulRetrieverService {
+
+	public static List<Item> getListOfItems(URI uri) {
+		Traverson traverson = new Traverson(uri, MediaTypes.HAL_JSON);
+		TraversalBuilder traversalBuilder = traverson.follow("items");
+		ParameterizedTypeReference<CollectionModel<Item>> parameterizedTypeReference;
+		parameterizedTypeReference = new ParameterizedTypeReference<>() {};
+		CollectionModel<Item> collectionModelOfItems;
+		collectionModelOfItems = traversalBuilder.toObject(parameterizedTypeReference);
+		Collection<Item> collectionOfItems = null;
+		if (collectionModelOfItems != null) {
+			collectionOfItems = collectionModelOfItems.getContent();
+		}
+		return new ArrayList<>(collectionOfItems);
+	}
+}
