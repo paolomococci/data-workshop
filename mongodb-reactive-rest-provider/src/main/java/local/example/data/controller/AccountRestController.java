c9bb8df1cf09f2c4240d59305474805cc478ecc0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-13 01:20:27 +0200

    Create AccountRestController.java

---
+++ mongodb-reactive-rest-provider/src/main/java/local/example/data/controller/AccountRestController.java
@@ -0,0 +1,64 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.data.controller;
+
+import local.example.data.document.Account;
+import local.example.data.repository.AccountRestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(value = "/accounts")
+public class AccountRestController {
+
+    @Autowired
+    private AccountRestRepository accountRestRepository;
+
+    @PostMapping
+    public Mono<Account> create(@RequestBody Account account) {
+        return this.accountRestRepository.save(account);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Mono<Account> read(@PathVariable String id) {
+        return this.accountRestRepository.findById(id);
+    }
+
+    @GetMapping
+    public Flux<Account> readAll() {
+        return this.accountRestRepository.findAll();
+    }
+
+    @PatchMapping(value = "/{id}")
+    public Mono<Account> patchUpdate(@RequestBody Account updated, @PathVariable String id) {
+        Account account = this.accountRestRepository.findById(id).block();
+        if (account != null) {
+            if (updated.getPassword() != null) account.setPassword(updated.getPassword());
+            return this.accountRestRepository.save(account);
+        }
+        return this.create(updated);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable String id) {
+        this.accountRestRepository.deleteById(id).subscribe();
+    }
+}
