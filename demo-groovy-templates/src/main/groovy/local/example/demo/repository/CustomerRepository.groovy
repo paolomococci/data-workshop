c473f127947394997359d916a88e055383ea371e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-10-31 19:57:40 +0100

    Create CustomerRepository.groovy

---
+++ demo-groovy-templates/src/main/groovy/local/example/demo/repository/CustomerRepository.groovy
@@ -0,0 +1,30 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.demo.repository
+
+import local.example.demo.model.Customer
+import org.springframework.data.repository.CrudRepository
+
+interface CustomerRepository
+        extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByName(String name)
+    List<Customer> findBySurname(String surname)
+    Customer findByEmail(String email)
+}
