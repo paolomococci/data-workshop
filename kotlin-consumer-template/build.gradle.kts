0c974a2e8cce57ab29acd449df029db5940228cd
Paolo Mococci <37364757+paolomococci@users.noreply.github.com>
2022-09-10 16:20:34 +0200

    Update build.gradle.kts

--- kotlin-consumer-template/build.gradle.kts
+++ kotlin-consumer-template/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 	mavenCentral()
 }
 
-extra["vaadinVersion"] = "14.8.16"
+extra["vaadinVersion"] = "14.8.17"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
